a9767457904d5905b20b891be331f8905ff1a523
vgtom <tom.vieillard.grelet@gmail.com>
2023-11-17 08:52:02 +0100

    test

--- model/DBModel.php
+++ model/DBModel.php
@@ -19,21 +19,21 @@ class DBModel
 		} 
 		catch(Exception $e) 
 		{
-			die('Erreur : '.$e->getMessage());
+			try
+			{
+				$server="DESKTOP-leon";
+				$dbname = "bdhotel_lepers-vieillard";
+				$user = "sa";
+				$password = "mdpsa";
+				$this->db = new PDO("sqlsrv:Server=$server;Database=$dbname", $user, $password);
+			}
+			catch(Exception $e) 
+			{
+				die('Erreur : '.$e->getMessage());
+			}
 		}		
 
-		try
-		{
-			$server="DESKTOP-leon";
-			$dbname = "bdhotel_lepers-vieillard";
-			$user = "sa";
-			$password = "mdpsa";
-			$this->db = new PDO("sqlsrv:Server=$server;Database=$dbname", $user, $password);
-		}
-		catch(Exception $e) 
-		{
-			die('Erreur : '.$e->getMessage());
-		}
+		
 	}
 	
 	public function getDb()
