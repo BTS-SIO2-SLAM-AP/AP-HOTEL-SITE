778f9ed98bd23c7eb81c2b52fe068268c52f6aef
vgtom <tom.vieillard.grelet@gmail.com>
2023-11-17 09:00:56 +0100

    test fix

--- model/DBModel.php
+++ model/DBModel.php
@@ -11,7 +11,7 @@ class DBModel
     {
 		try
 		{
-			$server = if(true) ? "VGTom\\SQLSERVEREXPRESS" : "DESKTOP-DOBTUVE";
+			$server = true ? "VGTom\\SQLSERVEREXPRESS" : "DESKTOP-DOBTUVE";
 			$dbname = "bdhotel_lepers-vieillard";
 			$user = "sa";
 			$password = "mdpsa";
