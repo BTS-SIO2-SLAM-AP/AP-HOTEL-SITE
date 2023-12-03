67c3bb0c80e2c71793000ef5acc6e10d8619a264
vgtom4 <tom.vieillard.grelet@gmail.com>
2023-12-03 15:31:29 +0100

    ajout méthodes dans ReservationM + ajustement save Méthodes : getReservation, getChambresReservation, getAllIdReservation

--- model/ReservationM.php
+++ model/ReservationM.php
@@ -6,6 +6,33 @@ require_once("DBModel.php");
 
 class ReservationM extends DBModel
 {
+    // Retourne toutes les informations d'une réservation en fonction de son numéro
+    public function getReservation($noresglobale)
+    {
+        $reqresult = parent::getDb()->prepare("select noresglobale, nohotel, nores, datedeb, datefin, nom, email, codeacces from reservation where noresglobale = $noresglobale");
+        $reqresult->execute();
+        $uneReservation = $reqresult->fetch();
+        $uneReservation["chambres"] = $this->getChambresReservation($noresglobale);
+        return $uneReservation;
+    }
+
+    // Retourne toutes les chambres d'une réservation en fonction de son numéro
+    public function getChambresReservation($noresglobale)
+    {
+        $reqresult = parent::getDb()->prepare("select reserver.nochambre from reserver inner join reservation on reserver.noresglobale = reservation.noresglobale where reservation.noresglobale=$noresglobale;");
+        $reqresult->execute();
+        return $reqresult->fetchAll();
+    }
+
+    // Retourne tous les numéros de réservation
+    public function getAllIdReservation()
+    {
+        $reqresult = parent::getDb()->prepare("select noresglobale from reservation");
+        $reqresult->execute();
+        return $reqresult->fetchAll(PDO::FETCH_COLUMN);
+    }
+
+    // Retourne le numéro de réservation suivant
     public function getNewNoRes($nohotel=0)
 	{
         $newNoRes = null;
@@ -30,25 +57,29 @@ class ReservationM extends DBModel
                 $req->execute();
 
                 $noResGlobale = parent::getDb()->lastInsertId();
-                $this->saveChambresReservation($noResGlobale, $nohotel, $lesChambres);
+                $reqChambreHasWorked = $this->saveChambresReservation($noResGlobale, $nohotel, $lesChambres);
             }catch (Exception $e) {
                 return false;
             }
-            return true;
+            return $reqChambreHasWorked ? $noResGlobale : 0;
         }
-        return false;
+        return 0;
     }
 
     public function saveChambresReservation($noResGlobale = 0, $nohotel = 0, $lesChambres = [])
 	{
         try {
-            $reqsql = "insert into reserver values ";
-            foreach ($lesChambres as $uneChambre) {
-                $reqsql .= "($nohotel, $uneChambre, $noResGlobale),";
-            }
-            $reqsql = substr($reqsql,0,-1);
-            $req = parent::getDb()->prepare($reqsql);
-            $req->execute();
+            // si la liste de chambres n'est pas vide
+            if (count($lesChambres) != 0) {
+                $reqsql = "insert into reserver values ";
+                foreach ($lesChambres as $uneChambre) {
+                    $reqsql .= "($nohotel, $uneChambre, $noResGlobale),";
+                }
+                $reqsql = substr($reqsql,0,-1);
+                $req = parent::getDb()->prepare($reqsql);
+                $req->execute();
+            } 
+            else return false;
         }catch (Exception $e) {
             return false;
         }
