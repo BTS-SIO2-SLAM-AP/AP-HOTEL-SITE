f94352a68149bb63bbf5b76843d3212967334966
vgtom <tom.vieillard.grelet@gmail.com>
2023-12-04 19:12:28 +0100

    nouvelle façon de filtrer par equipement (no work)

--- view/layouts/filtre.php
+++ view/layouts/filtre.php
@@ -1,6 +1,75 @@
 <link rel="stylesheet" href="assets/css/filterbar.css">
 <div class="filter-bar">
     <div class="content">
+        <div class="reservation-equipments-list">
+            <?php
+            foreach ($listEquipement as $unequ) { ?>
+                <label class="equipement-item">
+                    <input type="checkbox" checked>
+                    <img class="img-unchecked" src="assets/media/logo/handicapes.png">
+                    <img class="img-checked" src="assets/media/logo/<?php echo $unequ["imgequ"] ?>">
+                </label>
+
+                <!-- <input type="checkbox" name="equipement-<?php echo $unequ["noequ"] ?>" value="<?php echo $unequ["lib"] ?>" class="reservation__equipment__checkbox || icon--equipment__checkbox || tooltip-parent" id="equipement-<?php echo $unequ["noequ"] ?>" style="display: none;">
+                <label class="reservation__equipment__label || icon--equipment__label" for="equipement-<?php echo $unequ["noequ"] ?>" style="background-image: url('');">
+                </label> -->
+            <?php } ?>
+
+            <!-- <li class="reservation__equipments__item || icon--equipment">
+                <label class="reservation__equipment__label || icon--equipment__label">
+                    <input type="checkbox" name="animaux-admis" value="animaux-admis" class="reservation__equipment__checkbox || icon--equipment__checkbox || tooltip-parent"><span class="reservation__equipment__icon || icon--equipment__icon">
+                        <svg class="svg svg_i-animaux-admis" viewBox="0 0 44 44">
+                            <use xlink:href="/assets/svg/sprite.svg#svg-i-animaux-admis"></use>
+                        </svg></span>
+                    <span class="tooltip || icon--equipment__tooltip">animaux admis</span>
+                </label>
+            </li> -->
+        </div>
+
+
+
+        <!-- <script>
+            var equipementsList = document.querySelector(".reservation__equipments__list");
+            <?php
+            foreach ($listEquipement as $unequ) { ?>
+                <input type="checkbox" name="equipement-<?php echo $unequ["noequ"] ?>" value="<?php echo $unequ["lib"] ?>" class="reservation__equipment__checkbox || icon--equipment__checkbox || tooltip-parent" id="equipement-<?php echo $unequ["noequ"] ?>">
+                // var equipementItem = document.createElement("li");
+                // equipementItem.classList.add("reservation__equipments__item", "||", "icon--equipment");
+
+                // var equipementLabel = document.createElement("label");
+                // equipementLabel.classList.add("reservation__equipment__label", "||", "icon--equipment__label");
+
+                // var equipementCheckbox = document.createElement("input");
+                // equipementCheckbox.type = "checkbox";
+                // equipementCheckbox.name = "equipement-<?php echo $unequ["noequ"] ?>";
+                // equipementCheckbox.value = <?php echo json_encode($unequ["lib"]) ?>;
+                // equipementCheckbox.classList.add("reservation__equipment__checkbox", "||", "icon--equipment__checkbox", "||", "tooltip-parent");
+
+                // var equipementIcon = document.createElement("span");
+                // equipementIcon.classList.add("reservation__equipment__icon", "||", "icon--equipment__icon");
+
+                // var equipementSvg = document.createElement("svg");
+                // equipementSvg.classList.add("svg", "svg_i-<?php echo $unequ["imgequ"] ?>");
+                // equipementSvg.setAttribute("viewBox", "0 0 44 44");
+
+                // var equipementSvgUse = document.createElement("use");
+                // equipementSvgUse.setAttribute("xlink:href", "/assets/svg/sprite.svg#svg-i-<?php echo $unequ["imgequ"] ?>");
+
+                // var equipementTooltip = document.createElement("span");
+                // equipementTooltip.classList.add("tooltip", "||", "icon--equipment__tooltip");
+                // equipementTooltip.textContent = <?php echo json_encode($unequ["lib"]) ?>;
+
+                // equipementSvg.appendChild(equipementSvgUse);
+                // equipementIcon.appendChild(equipementSvg);
+                // equipementLabel.appendChild(equipementCheckbox);
+                // equipementLabel.appendChild(equipementIcon);
+                // equipementLabel.appendChild(equipementTooltip);
+                // equipementItem.appendChild(equipementLabel);
+                // equipementsList.appendChild(equipementItem);
+            <?php } ?>
+        </script> -->
+
+
         <div class="filter-equipements">
             Sélectionnez un équipement :
             <select name="equipements-list[]" id="equipements-list" onchange="filterHotels()" multiple>
@@ -9,7 +78,7 @@
                 <?php } ?>
             </select>
         </div>
-        
+
         <div class="filter-prix">
             <label for="price-range">Prix maximum :</label>
             <input type="range" id="price-range" name="price_range" min="0" max="<?php echo $prixMax; ?>" value="<?php echo $prixMax; ?>" oninput="updatePriceLabel()">
@@ -127,9 +196,9 @@
             }
 
             function resetFilters() {
-                
+
                 // get valeur par défaut du range
-                document.getElementById("price-range").value = <?php echo $prixMax; ?>; 
+                document.getElementById("price-range").value = <?php echo $prixMax; ?>;
                 updatePriceLabel();
                 document.getElementById("city-search").value = "";
                 document.getElementById("equipements-list").value = [];
