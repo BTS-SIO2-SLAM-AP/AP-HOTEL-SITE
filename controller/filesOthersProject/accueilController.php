afc6f2c0b27bc4bc6ccc5d13f52e7f9e91e06d04
vgtom4 <tom.vieillard.grelet@gmail.com>
2023-12-05 14:29:46 +0100

    clean project

--- controller/filesOthersProject/accueilController.php
+++
@@ -1,7 +0,0 @@
-<?php
-// CONTROLEUR de l'affichage d'accueil
-
-// Aucun traitement donc affichage de la vue
-require_once 'view/accueilView.php';
-
-//Absence volontaire de la balise fermeture php (préconisation)
